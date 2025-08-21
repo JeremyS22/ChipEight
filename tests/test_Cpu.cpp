fa621c17306604620b9b1768384a28e1d36191e3
Jeremy Sloan <JerSloan622@gmail.com>
2025-08-20 23:07:29 -0400

    Initially created test file with gtest

---
+++ tests/test_Cpu.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h> 
+#include <Cpu.h> 
+#include <Debugger.h>
+
+ 
+
+TEST(testingSuite1, test1){
+    Debugger debugger; 
+    Cpu cpu(debugger); 
+    cpu.setRegist_V(0, 3);
+    cpu.setRegist_V(1, 6);
+    int secondNibble = 0; 
+    int thirdNibble = 1; 
+
+    cpu.setVXToValueOfVY(secondNibble, thirdNibble); 
+    EXPECT_EQ(cpu.getRegist_V(0), 6); 
+}
+
+TEST(testingSuite1, test2){
+    EXPECT_EQ(3+3, 6); 
+}
+
