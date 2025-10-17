7769e9ec8f4f42e076e99e454047b65ae435d365
Jeremy Sloan <JerSloan622@gmail.com>
2025-10-16 23:40:45 -0400

    Add more unit tests for Screen class, refactor existing tests

--- tests/test_Screen.cpp
+++ tests/test_Screen.cpp
@@ -14,30 +14,77 @@ class ScreenTest : public testing::Test {
     Screen screen; 
     Debugger debugger; 
     Cpu cpu; 
+
+    void testPrep_TurnAllPixelsOn(){
+        for(int i = 0; i < 64; ++i){
+            for(int j = 0; j < 32; ++j){
+                screen.setPixelStatus(i,j,true,cpu);         
+            }
+        }        
+    }
 }; 
 
+TEST_F(ScreenTest, getPixelStatus_ReturnsFalse_WhenPixelIsFalse){
+    const bool value = false; 
+    screen.setPixelStatus(16, 28, value, cpu); 
+
+    EXPECT_FALSE(screen.getPixelStatus(16,28)); 
+}
+
+TEST_F(ScreenTest, getPixelStatus_ReturnsTrue_WhenPixelIsTrue){
+    const bool value = true; 
+    screen.setPixelStatus(16, 28, value, cpu); 
+
+    EXPECT_TRUE(screen.getPixelStatus(16,28)); 
+}
+
 TEST_F(ScreenTest, setPixelStatus_PixelWillBeSetToTrue_WhenValueTrue_AndExistingPixelFalse){
-    bool value = true; 
+    const bool value = true; 
     screen.setPixelStatus(20, 26, value, cpu); 
 
-    EXPECT_EQ(screen.getPixelStatus(20,26), true); 
+    EXPECT_TRUE(screen.getPixelStatus(20,26)); 
 }
 
 TEST_F(ScreenTest, setPixelStatus_PixelWillBeSetToFalse_WhenBothValueAndExistingPixelAreTrue){
-    bool value = true; 
+    const bool value = true; 
     screen.setPixelStatus(20, 26, value, cpu); 
 
     screen.setPixelStatus(20, 26, value, cpu); 
 
-    EXPECT_EQ(screen.getPixelStatus(20,26), false);
+    EXPECT_FALSE(screen.getPixelStatus(20,26));
 }
 
 TEST_F(ScreenTest, setPixelStatus_VFSetToOne_WhenBothValueAndExistingPixelAreTrue){
-    bool value = true; 
+    const bool value = true; 
     screen.setPixelStatus(20, 26, value, cpu); 
 
     screen.setPixelStatus(20, 26, value, cpu); 
 
     EXPECT_EQ(cpu.getRegist_V(0xF), 1) << "Incorrect value in register VF, check flag register implementation"; 
+}
+
+TEST_F(ScreenTest, turnOffAllPixels_EveryPixelSetToFalse_WhenAllPixelsInitiallyTrue){
+    testPrep_TurnAllPixelsOn(); 
+
+    screen.turnOffAllPixels(cpu); 
+
+    for(int i = 0; i < 64; ++i){
+        for(int j = 0; j < 32; ++j){
+            EXPECT_FALSE(screen.getPixelStatus(i,j)) << "Incorrect Pixel set to true.  Where x is " << i << " and y is " << j; 
+        }
+    }
+}
+
+TEST_F(ScreenTest, initializeScreen_EachSDLSubsystemsIsInitialized){
+
+    screen.initializeScreen(); 
+    
+    // SDL_WasInit() returns 0 if subsystem wasn't initialized 
+    EXPECT_NE(SDL_WasInit(SDL_INIT_TIMER), 0); 
+    EXPECT_NE(SDL_WasInit(SDL_INIT_AUDIO), 0); 
+    EXPECT_NE(SDL_WasInit(SDL_INIT_VIDEO), 0); 
+    EXPECT_NE(SDL_WasInit(SDL_INIT_JOYSTICK), 0); 
+    EXPECT_NE(SDL_WasInit(SDL_INIT_GAMECONTROLLER), 0); 
+    EXPECT_NE(SDL_WasInit(SDL_INIT_EVENTS), 0); 
 }
 
